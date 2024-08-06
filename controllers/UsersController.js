4e25277341f9d95af5a70e1b7832388aedd86aba
Mrelshimy <mraafat.elsayed@gmail.com>
2024-08-06 14:46:18 +0300

    bugfix

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -2,33 +2,32 @@ const sha1 = require('sha1');
 const dbClient = require('../utils/db');
 
 class UsersController {
-  static postNew(request, response) {
-    const { email } = request.body;
-    const { password } = request.body;
+  static postNew(req, res) {
+    const { email } = req.body;
+    const { password } = req.body;
 
     if (!email) {
-      response.status(400).json({ error: 'Missing email' });
-      return;
+      res.status(400).json({ error: 'Missing email' });
     }
+
     if (!password) {
-      response.status(400).json({ error: 'Missing password' });
-      return;
+      res.status(400).json({ error: 'Missing password' });
     }
 
     const users = dbClient.db.collection('users');
     users.findOne({ email }, (err, user) => {
       if (user) {
-        response.status(400).json({ error: 'Already exist' });
+        res.status(400).json({ error: 'Already exist' });
       } else {
-        const hashedPassword = sha1(password);
-        users.insertOne(
-          {
-            email,
-            password: hashedPassword,
-          },
-        ).then((result) => {
-          response.status(201).json({ id: result.insertedId, email });
-        }).catch((error) => console.log(error));
+        const userPassword = sha1(password);
+        users.insertOne({
+          email,
+          password: userPassword,
+        }).then((result) => {
+          res.status(201).json({ id: result.insertedId, email });
+        }).catch((err) => {
+          console.log(err);
+        });
       }
     });
   }
