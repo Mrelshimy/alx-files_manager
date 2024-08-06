ea1de1b37482dbfcd6ac8ed08bbedae01356123a
Mrelshimy <mraafat.elsayed@gmail.com>
2024-08-06 13:09:23 +0300

    task3

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,8 +1,10 @@
-const redisClient = require('../utils/redis');
+import { response } from 'express';
+import sha1 from 'sha1';
+
 const dbClient = require('../utils/db');
 
 class UserController {
-  static newUser(req, res) {
+  static postNew(req, res) {
     const { email } = req.body;
     const { password } = req.body;
 
@@ -13,7 +15,23 @@ class UserController {
     if (!password) {
       res.status(400).json({ error: 'Missing password' });
     }
+
+    const users = dbClient.db.collection('users');
+    const user = users.findOne({ email });
+    if (user) {
+      res.status(400).json({ err: 'Already exist' });
+    } else {
+      const userPassword = sha1(password);
+      users.addOne({
+        email,
+        password: userPassword,
+      }).then((result) => {
+        response.status(201).json({ id: result.insertedId, email });
+      }).catch((err) => {
+        console.log(err);
+      });
+    }
   }
 }
 
-module.exports = UsersController;
+module.exports = UserController;
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,6 @@ const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.post('/users', UserController);
+router.post('/users', UserController.postNew());
 
 module.exports = router;
